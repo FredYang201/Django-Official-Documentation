89ff8fbffe8aa99a89cd177a7aaf336074952ff8
FredYang201 <fredyang1402143595@gmail.com>
2021-02-21 02:35:19 -0800

    Topic 1 -- Tutorial -- Poll App

---
+++ Chapter 1 - Poll/mysite/poll/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, re_path, include
+from . import views
+
+# extra_patterns = [
+#     path("reports/", credit_views.report),
+#     path("reports/<int:num>/", credit_views.report),
+#     path('charges/', credit_views.charge)
+# ]
+
+app_name = 'poll'
+urlpatterns = [
+#     path('credit/', include(extra_patterns)),
+#     re_path(r'^(?P<year>[0-9]{4})/$', views.index),
+#     re_path(r'articles/(?:page-(?P<page_number>\d+)/)?$', views.index)
+    # path('', views.index, name='index'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/result', views.result, name='result'),
+    path('<int:question_id>/vote', views.vote, name='vote'),
+
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:question_id>/result', views.result, name='result'),
+    path('<int:question_id>/vote', views.vote, name='vote'),
+]
